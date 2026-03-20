d0dd68c72d97b94d52689715a01958c89cc81991
thomas Marchal <thomasmarchal@Air-de-thomas.lan>
2026-03-20 22:08:23 +0800

    add scope enedis

--- app/api/enedis/connect/route.ts
+++ app/api/enedis/connect/route.ts
@@ -16,14 +16,14 @@ export async function GET() {
   }
 
   const url = new URL(
-    "https://gw.hml.api.enedis.fr/group/espace-particuliers/consentement-linky/oauth2/authorize"
-  );
+  "https://gw.ext.prod-sandbox.api.enedis.fr/oauth2/v3/authorize"
+);
 
-  url.searchParams.set("client_id", clientId);
-  url.searchParams.set("response_type", "code");
-  url.searchParams.set("redirect_uri", redirectUri);
-  url.searchParams.set("state", "unifee");
-  url.searchParams.set("duration", "P3Y");
+url.searchParams.set("client_id", clientId);
+url.searchParams.set("response_type", "code");
+url.searchParams.set("redirect_uri", redirectUri);
+url.searchParams.set("scope", "openid");
+url.searchParams.set("state", "unifee");
 
   return NextResponse.redirect(url.toString());
 }
